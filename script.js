49fd12332a2101bdb18b64f2c12c3c9e334ee994
JosueDeLosSantos <jdd1993@hotmail.com>
2023-03-20 18:17:53 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,74 @@
+function createNode(value = null) {
+  return {
+    value: value,
+    next: null,
+  };
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  // adds a new node containing value to the end of the list
+  append(value) {
+    this.length++;
+    let newNode = createNode(value);
+
+    if (this.tail) {
+      this.tail.next = newNode;
+      this.tail = newNode;
+      return newNode;
+    }
+
+    this.head = this.tail = newNode;
+    return newNode;
+  }
+
+  // adds a new node containing value to the start of the list
+  prepend(value) {
+    this.length++;
+    let newNode = createNode(value);
+    this.head = { value: value, next: this.head };
+    return newNode;
+  }
+
+  // returns the total number of nodes in the list
+  size() {
+    return this.length;
+  }
+
+  // returns the first node in the list
+  headNode() {
+    return this.head;
+  }
+
+  // returns the last node in the list
+  tailNode() {
+    return this.tail;
+  }
+
+  // returns the node at the given index
+  at(index) {
+    let indexValue = this.head;
+    for (let i = 1; i <= index - 1; i++) {
+      indexValue = indexValue.next;
+    }
+    return indexValue;
+  }
+}
+
+const linkedList = new LinkedList();
+
+linkedList.append(7);
+linkedList.append(8);
+linkedList.append(true);
+linkedList.append(20);
+linkedList.append(21);
+linkedList.prepend(6);
+linkedList.prepend(5);
+// linkedList.head = { value: 6, next: linkedList.head };
+console.log(linkedList);
+console.log(linkedList.at(7));
